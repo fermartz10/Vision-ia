80ab9db44cdd159e494413c89555e98df55a3b79
Fernando Martinez <35785337+fermartz10@users.noreply.github.com>
2024-03-28 10:38:01 -0600

    Modelo yolo

--- pose.py
+++ pose.py
@@ -1,34 +1,11 @@
-import cv2
-import mediapipe as mp
+def detector_pose():   
+    from ultralytics import YOLO
+    model = YOLO ('yolov8n-pose.pt')
+    results = model(source = 0, show= True, conf = 0.3)
 
-## inicializar el estimador de posturas
-mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
-pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+detector_pose()
 
-source = cv2.VideoCapture(0)
-while source.isOpened():
-    # leer el fotograma
-    _, frame = source.read()
-    try:
-         # redimensionar el fotograma para video en vertical
-         # frame = cv2.resize(frame, (350, 600))
-         # convertir a RGB
-         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-         
-         # procesar el fotograma para detección de posturas
-         pose_results = pose.process(frame_rgb)
-         # print(pose_results.pose_landmarks)
-         
-         # dibujar el esqueleto en el fotograma
-         mp_drawing.draw_landmarks(frame, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-         # mostrar el fotograma
-         cv2.imshow('Salida', frame)
-    except:
-         break
-    
-    if cv2.waitKey(1) == ord('q'):
-          break
-          
-source.release()
-cv2.destroyAllWindows()
+
+'''Como resolución de cámara predeterminada, su navegador utiliza 640×480 (0.307MP).
+        La resolución máxima admitida de su cámara es 1280×720 (0.922MP).'''
+
